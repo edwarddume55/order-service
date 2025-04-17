14b517fa6557b6f623633b44f74e881af21f0cbb
Edward Dume <edwarddume55@gmail.com>
2025-04-17 15:12:44 +0300

    connecting to render database and making the credatial secure using the .env

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+load_dotenv() 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z*x6wi%#w_-uq@ql(@e7syr&m)kh@ob0%f@0xp0opwvoc&ftfh'
-
+SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
+
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -77,13 +79,24 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# Database configuration
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
--- orders/tests/test_views.py
+++ orders/tests/test_views.py
@@ -3,9 +3,13 @@ from rest_framework.test import APITestCase
 from rest_framework import status
 from customers.models import Customer
 
+from django.contrib.auth.models import User
+
 
 class OrderAPITests(APITestCase):
     def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+        self.client.force_authenticate(user=self.user)
         self.customer = Customer.objects.create(
             code="CUST001",
             name="Test Customer",
@@ -15,7 +19,7 @@ class OrderAPITests(APITestCase):
         self.order_data = {
             "customer": self.customer.id,
             "item": "Test Item",
-            "amount": "1000.00"
+            "amount": 1000.00
         }
     def test_create_order(self):
         url = reverse('order-list')
